3d1d4f2fada884386c28733c336c49baa1bb04c1
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-17 19:35:56 -0300

    ending challenge

--- dio-challenge-POO/src/Main.java
+++ dio-challenge-POO/src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.challenge.domain.Content;
-import br.com.dio.challenge.domain.Course;
-import br.com.dio.challenge.domain.Mentoring;
+import br.com.dio.challenge.domain.*;
 
 import java.time.LocalDate;
 
@@ -26,9 +24,43 @@ public class Main {
         mentoring.setDescription("Java Description Mentoring");
         mentoring.setDate(LocalDate.now());
 
-        System.out.println(course1);
-        System.out.println(course2);
-        System.out.println(mentoring);
+//        System.out.println(course1);
+//        System.out.println(course2);
+//        System.out.println(mentoring);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Java Developer Bootcamp");
+        bootcamp.setDescription("Java Developer Bootcamp Description");
+        bootcamp.getContents().add(course1);
+        bootcamp.getContents().add(course2);
+        bootcamp.getContents().add(mentoring);
+
+        Dev devCamila = new Dev();
+        devCamila.setName("Camila");
+        devCamila.subscribeBootcamp(bootcamp);
+        System.out.println("Subscribed Contents Camila" + devCamila.getSubscribedContents());
+        devCamila.progress();
+        devCamila.progress();
+        System.out.println("-");
+        System.out.println("Subscribed Contents Camila" + devCamila.getSubscribedContents());
+        System.out.println("Completed Contents Camila" + devCamila.getCompletedContents());
+        System.out.println("XP: " + devCamila.calculateTotalXp());
+
+
+        System.out.println("------------");
+        Dev devJohn = new Dev();
+        devJohn.setName("John");
+        devJohn.subscribeBootcamp(bootcamp);
+        System.out.println("Subscribed Contents John" + devJohn.getSubscribedContents());
+        devJohn.progress();
+        devJohn.progress();
+        devJohn.progress();
+        System.out.println("-");
+        System.out.println("Subscribed Contents John" + devJohn.getSubscribedContents());
+        System.out.println("Completed Contents John" + devJohn.getCompletedContents());
+        System.out.println("XP: " + devJohn.calculateTotalXp());
+
+
 
     }
 }
